d1db845bb4ce55a3edc272b69432bed175906d41
Tom Martensen <tom.martensen@student.hpi.de>
2017-05-16 21:02:30 +0200

    implemented backend mock

---
+++ src/Utils/MockData/Hierarchy.js
@@ -0,0 +1,9 @@
+const Hierarchy = [
+    {
+        "id": 2,
+        "name": "Hierarchie Bosch Thermotechnik",
+        "timestamp": "2017-05-16T18:33:51.104Z"
+    }
+];
+
+export default Hierarchy;
