164ace07374838fb258cf1e3263fcee4d34dc02e
Tom Martensen <tom.martensen@student.hpi.de>
2017-05-16 21:06:14 +0200

    improves backend mock

--- src/Utils/MockData/Hierarchy.js
+++ src/Utils/MockData/Hierarchy.js
@@ -1,8 +1,18 @@
 const Hierarchy = [
     {
         "id": 2,
-        "name": "Hierarchie Bosch Thermotechnik",
+        "name": "Transport for London - Event Queries",
         "timestamp": "2017-05-16T18:33:51.104Z"
+    },
+    {
+        "id": 3,
+        "name": "Bosch Thermotechnik - Ereignisabfragen",
+        "timestamp": "2017-05-16T19:01:16.048Z"
+    },
+    {
+        "id": 4,
+        "name": "Fast Flower Delivery Application",
+        "timestamp": "2017-05-15T20:01:16.048Z"
     }
 ];
 
