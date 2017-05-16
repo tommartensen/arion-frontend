52ab44300f0f495828dc8f08e77a0bf4ee568111
Tom Martensen <tom.martensen@student.hpi.de>
2017-05-16 22:50:41 +0200

    include floating button

--- src/ListView/ListView.js
+++ src/ListView/ListView.js
@@ -1,16 +1,25 @@
 import React, {Component} from "react";
-import {Card, CardText, CardTitle} from "material-ui";
+import {Card, CardText, CardTitle, FloatingActionButton} from "material-ui";
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import { css } from 'aphrodite';
 import HierarchyList from "./HierarchyList";
+import ListViewStyles from "./ListViewStyles";
 
 class ListView extends Component {
     render() {
         return (
-            <Card>
-                <CardTitle title="Hierarchies" subtitle="You can see all event query hierarchies here." />
-                <CardText>
-                    <HierarchyList/>
-                </CardText>
-            </Card>
+            <div>
+                <Card>
+                    <CardTitle title="Hierarchies" subtitle="You can see all event query hierarchies here." />
+                    <CardText>
+                        <HierarchyList/>
+                    </CardText>
+                </Card>
+                <FloatingActionButton
+                    className={css(ListViewStyles.floatingActionButton)}>
+                    <ContentAdd />
+                </FloatingActionButton>
+            </div>
         );
     }
 }
