5e4b418aa975bfc5f8fa69b02a357e5d250e80d2
Tom Martensen <tom.martensen@student.hpi.de>
2017-05-16 19:36:23 +0200

    basic most important views

---
+++ src/ListView/ListView.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class ListView extends Component {
+    render() {
+        return (
+            <div>
+                ListView
+            </div>);
+    }
+}
+
+export default ListView;
