db478a2061fd25084d5af181fee81698934e22e9
Tom Martensen <tom.martensen@student.hpi.de>
2017-05-16 19:36:03 +0200

    created config template

---
+++ src/config/config-template.js
@@ -0,0 +1,23 @@
+const config = {
+    projectName: "Arion EQMN Modeler",
+    useBackendMock: false,
+    backendRESTRoute: "http://localhost:3000/",
+    colors: {
+        primaryDark: "#000051",
+        primary: "#1a237e",
+        primaryLight: "#534bae",
+        accent: "#C99700",
+        accentLight: "#ffc107",
+        accentDark: "#7A5C00",
+        text: "#424242",
+        textAlternate: "#ffffff",
+        border: "#e0e0e0",
+        diagramLine: '#9E9E9E',
+        diagramBackground: "rgba(224, 224, 224, 0.6)",
+        error: "#B71C1C",
+    },
+    descriptions: {},
+    messages: {},
+};
+
+export default config;
