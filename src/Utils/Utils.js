70a5b6cb135e544e554cdba44d1053f6067aaef3
Tom Martensen <mail@tommartensen.de>
2017-05-17 11:50:36 +0200

    add pretty print json method

--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -1,8 +1,18 @@
+import React from "react";
+
 class Utils {
     static parseTimestamp(timestamp) {
         const date = timestamp.split("T")[0];
         const time = timestamp.split("T")[1];
         return date + " at " + time.split(".")[0]
+    }
+
+    static prettyPrintJSON(object) {
+        return (
+            <div style={{whiteSpace: "pre"}}>
+                {JSON.stringify(object, null, '\t')}
+            </div>
+        );
     }
 }
 
