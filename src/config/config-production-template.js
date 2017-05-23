c4627dd22312075995570b83ec44fb797f30ff91
Tom Martensen <tom.martensen@student.hpi.de>
2017-05-23 21:03:44 +0200

    create new config template for production

---
+++ src/config/config-production-template.js
@@ -0,0 +1,25 @@
+const config = {
+    projectName: "Arion EQMN Modeler",
+    useBackendMock: false,
+    backendRESTRoute: "${HOST}",
+    colors: {
+        primaryDark: "#33691E",
+        primary: "#558B2F",
+        primaryLight: "#8BC34A",
+        accent: "#CDDC39",
+        accentLight: "#D4E157",
+        accentDark: "#9E9D24",
+        text: "#424242",
+        textAlternate: "#ffffff",
+        border: "#e0e0e0",
+        diagramLine: '#9E9E9E',
+        diagramBackground: "rgba(224, 224, 224, 0.6)",
+        error: "#B71C1C",
+    },
+    descriptions: {
+        toolTipToDetailView: "See Event Query Hierarchy",
+    },
+    messages: {},
+};
+
+export default config;
