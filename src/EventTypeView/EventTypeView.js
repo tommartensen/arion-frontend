b3606c262bf8638080e889fb6881f8192523b684
Tom Martensen <tom.martensen@student.hpi.de>
2017-05-30 17:51:12 +0200

    Implemented EventTypeView

--- src/EventTypeView/EventTypeView.js
+++ src/EventTypeView/EventTypeView.js
@@ -2,22 +2,109 @@ import React from "react";
 import {connect, PromiseState} from "react-refetch";
 import ConnectionComponent from "../Utils/ConnectionComponent";
 import config from "../config/config";
+import { css }from 'aphrodite';
+import {Col, Row} from "react-grid-system";
+import AppStyles from "../AppStyles";
+import {Card, CardText, CardTitle, IconButton, List, ListItem} from "material-ui";
+import IconSearch from 'material-ui/svg-icons/action/search';
+import Utils from "../Utils/Utils";
 
 class EventTypeView extends ConnectionComponent {
+    static renderEventQueryDetailButton(eventQueryId) {
+        return (
+            <IconButton
+                tooltip={config.descriptions.tooltipToEventQueryInEQMN}
+                href={"/query/" + eventQueryId}>
+                <IconSearch/>
+            </IconButton>
+        );
+    }
+
+    static renderEventQueryList(eventQueries) {
+        return (
+            <List>
+                {
+                    eventQueries.map((eventQuery, key) => {
+                        return (
+                            <ListItem
+                                key={key}
+                                primaryText={eventQuery.query}
+                                disabled={true}
+                                rightIconButton={EventTypeView.renderEventQueryDetailButton(eventQuery.id)}
+                            />
+                        );
+                    })
+                }
+            </List>
+        );
+    }
+
+    static generateEventTypeModel(eventQueries) {
+        const eqmn_representation = [];
+        eventQueries.forEach(eventQuery => {
+            eqmn_representation.push(
+                Utils.prettyPrintJSON(
+                    {
+                        query: eventQuery.eqmn_representation["input"],
+                        condition: eventQuery.eqmn_representation["condition"]
+                    },
+                )
+            );
+        });
+        return (
+            <div>
+                {
+                    eqmn_representation.map(representation => {
+                        return representation;
+                    })
+                }
+            </div>
+        );
+    }
+
     render() {
-        const connectionIncomplete = super.render(PromiseState.all([this.props.eventType]));
+        const connectionIncomplete = super.render(PromiseState.all([this.props.eventType, this.props.eventQueries]));
         if (connectionIncomplete) {
             return connectionIncomplete;
         }
         const eventType = this.props.eventType.value;
+        const eventQueries = this.props.eventQueries.value;
         return (
             <div>
                 <h1>{eventType.name}</h1>
+                <Row className={css(AppStyles.marginTop30)}>
+                    <Col md={12}>
+                        <Card>
+                            <CardTitle title={"Feeding Queries"}/>
+                            <CardText>
+                                {EventTypeView.renderEventQueryList(eventQueries)}
+                            </CardText>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className={css(AppStyles.marginTop30)}>
+                    <Col md={12}>
+                        <Card>
+                            <CardTitle title={"EQMN Representation"}/>
+                            <CardText>
+                                {EventTypeView.generateEventTypeModel(eventQueries)}
+                            </CardText>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className={css(AppStyles.marginTop30)}>
+                    <Col md={12}>
+                        <Card>
+                            <CardTitle title={"EQMN Model"}/>
+                        </Card>
+                    </Col>
+                </Row>
             </div>
         );
     }
 }
 
 export default connect.defaults({fetch: ConnectionComponent.switchFetch})(props => ({
-    eventType: config.backendRESTRoute + `/api/event_type/esper/${props.match.params.eventTypeId}`
+    eventType: config.backendRESTRoute + `/api/event_type/esper/${props.match.params.eventTypeId}`,
+    eventQueries: config.backendRESTRoute + `/api/query/esper/event_type/${props.match.params.eventTypeId}`
 }))(EventTypeView);
